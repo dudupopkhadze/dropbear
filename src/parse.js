da22968672049bdb4d838f194c77669a2a1a5bc7
Dudu Popkhadze <dudupopkhadze@gmail.com>
2022-09-24 18:59:46 +0400

    parse

--- src/parse.js
+++ src/parse.js
@@ -2,8 +2,53 @@ const { isOpeningParenthesis, isClosingParenthesis } = require('./identify');
 const { specialForms } = require('./special-forms');
 const { peek, pop } = require('./utilities');
 
-const parenthesize = () => {};
+const parenthesize = (tokens) => {
+  const token = pop(tokens);
 
-const parse = () => {};
+  if (isOpeningParenthesis(token.value)) {
+    const exprs = [];
+    while (!isClosingParenthesis(peek(tokens).value)) {
+      exprs.push(parenthesize(tokens));
+    }
+    pop(tokens);
 
-module.exports = { parse: tokens => parse(parenthesize(tokens)) };
+    return exprs;
+  }
+
+  return token;
+};
+
+const parse = (tokens) => {
+  if (Array.isArray(tokens)) {
+    const [first, ...rest] = tokens;
+    return {
+      type: 'CallExpression',
+      name: first.value,
+      arguments: rest.map(parse),
+    };
+  }
+  const token = tokens;
+
+  if (token.type === 'Number') {
+    return {
+      type: 'NumericLiteral',
+      value: token.value,
+    };
+  }
+
+  if (token.type === 'String') {
+    return {
+      type: 'StringLiteral',
+      value: token.value,
+    };
+  }
+
+  if (token.type === 'Name') {
+    return {
+      type: 'Identifier',
+      name: token.value,
+    };
+  }
+};
+
+module.exports = { parse: (tokens) => parse(parenthesize(tokens)) };
